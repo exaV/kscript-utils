777afe441809098d6de1c8637f8081edf6e011c1
Patrick Del Conte <patrick.delconte@umb.ch>
2019-04-02 14:24:12 +0200

    re-export kutils with an evalBash extension function on string

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `java-library`
     kotlin("jvm") version "1.3.21"
     maven
 }
 
 group = "ch.delconte.kscript-utils"
-version = "0.1"
+version = "0.2"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation("de.mpicbg.scicomp:kutils:0.11")
+    api("de.mpicbg.scicomp:kutils:0.11")
     implementation("com.xenomachina:kotlin-argparser:2.0.7")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/Bash.kt
+++ src/main/kotlin/Bash.kt
@@ -1,26 +1,11 @@
 package ch.delconte.kscript.utils
 
+import de.mpicbg.scicomp.kutils.evalBash
 import java.io.File
 
-fun evalBash(cmd: String, workingDir: String = ".", showOutput: Boolean = true, printCmd: Boolean = false): Int {
-    val dir = File(workingDir)
-    if (printCmd) {
-        println("${dir.absolutePath}$ $cmd")
-    }
-
-    var builder = ProcessBuilder("/bin/sh", "-c", cmd)
-        .directory(dir)
-
-    if (showOutput) {
-        builder = builder.inheritIO()
-    }
-
-    val returnValue = builder.start().waitFor()
-
-    return returnValue
-}
-
 @JvmName("evalBashStatic")
-fun String.evalBash(workingDir: String = ".", showOutput: Boolean = true, printCmd: Boolean = false) =
-    evalBash(this, workingDir, showOutput, printCmd)
+fun String.evalBash(
+    showOutput: Boolean = false,
+    redirectStdout: File? = null, redirectStderr: File? = null, wd: File? = null
+) = evalBash(this, showOutput, redirectStdout, redirectStderr)
 
