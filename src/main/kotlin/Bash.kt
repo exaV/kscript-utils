a49a4cbb69adf00806e56a255db33e7b1a537325
Patrick Del Conte <patrick.delconte@umb.ch>
2019-04-25 17:31:58 +0200

    add evalBash without receiver and language 'Bash'

--- src/main/kotlin/Bash.kt
+++ src/main/kotlin/Bash.kt
@@ -1,9 +1,12 @@
 package ch.delconte.kscript.utils
 
-import de.mpicbg.scicomp.kutils.evalBash
 import org.intellij.lang.annotations.Language
 import java.io.File
+import de.mpicbg.scicomp.kutils.evalBash as eval
+
+fun evalBash(@Language("Bash") cmd: String, showOutput: Boolean = false, wd: File? = null) =
+    eval(cmd, showOutput, wd)
 
 @JvmName("evalBashStatic")
 fun @receiver:Language("Bash") String.evalBash(showOutput: Boolean = false, wd: File? = null) =
-    evalBash(this, showOutput, wd)
+    eval(this, showOutput, wd)
